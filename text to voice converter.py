f5a544a8fb0ab4daf26bade3e2eb2f2a978be323
rawatsrijan <91362387+rawatsrijan@users.noreply.github.com>
2022-07-17 09:32:42 +0530

    Update text to voice converter.py

--- text to voice converter.py
+++ text to voice converter.py
@@ -2,24 +2,24 @@ from tkinter import *
 import pyttsx3
 root = Tk()
 root.title("srijan rawat mini project")
-label=Label(root,text="MINI ProJect\n TEXT TO VOICE CONVERTER ")
+label=Label(root,text="MINI ProJect\n \n\nTEXT TO VOICE CONVERTER\n\n\n\n ",bg="white",fg="orange",width=112,height=9)
 label.pack()
 root.minsize(width=1000,height=5000)
 root.maxsize(width=1000,height=8000)
-l = Label(root,text="Enter the word here-->>",bg="pink")
-l.place(x=0,y=40)
-e = Entry(root, width=20)
-e.place(x=160,y=40)
+l = Label(root,text="Enter the word here-->>",bg="pink",height=10)
+l.place(x=0,y=590)
+e = Entry(root, width=30)
+e.place(x=180,y=658)
 def srijan():
     a=pyttsx3.init()
     a.say(e.get())
     a.runAndWait()
-b=Button(root,text="enter",bg="red",fg="red",command=srijan)
-b.place(x=210,y=70)
+b=Button(root,text="SPEAK",bg="red",fg="black",command=srijan)
+b.place(x=270,y=700)
 
 p= PhotoImage(file="/Users/srijanrawat/Desktop/r.png")
 o = Label(root,image=p,width=788,height=410)
-o.place(x=10,y=200)
+o.place(x=10,y=150)
 
 
 root.mainloop()
